8bdcc3e2705d53ca0ba50184e6c9b35a8634373a
Yo sup <dhdytjq2@gamil.com>
2023-01-08 16:08:04 +0900

    react-hook-form

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,48 +1,100 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// function TodoList() {
-//   const [toDo, setToDo] = useState("");
-//   const [toDoError, setToDoError] = useState("");
-//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = e;
-//     setToDo(value);
-//     setToDoError("");
-//   };
-
-//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     if (toDo.length < 10) {
-//       setToDoError("To do should be at least 10");
-//     } else if (toDo.length > 10) {
-//       setToDoError("");
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={onSubmit}>
-//       <input value={toDo} onChange={onChange} placeholder="Write to do" />
-//       <button type="submit">Add</button>
-//       {toDoError !== "" ? toDoError : ""}
-//     </form>
-//   );
-// }
+interface IForm {
+  email: string;
+  FirstName: string;
+  LastName: string;
+  userName: string;
+  Password: string;
+  Password1: string;
+  extraError?: string;
+}
 
 function TodoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch
+  } = useForm<IForm>({
+    defaultValues: { email: "@naver.com" },
+  });
+  //react-hook-form이 모든 validation을 마쳤을 때만 호출된다.
+
+  const onValid = (data: IForm) => {
+    if (data.Password !== data.Password1) {
+      setError(
+        "Password1",
+        {
+          message: "Password is not the same as Password",
+        },
+        { shouldFocus: true }
+      );
+      setError("extraError", { message: "Server Offline" });
+    }
+  };
+
+  console.log(errors)
+
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("FirstName")} placeholder="FirstName" />
-        <input {...register("LastName")} placeholder="LastName" />
-        <input {...register("userName")} placeholder="userName" />
-        <input {...register("Password")} placeholder="Password" />
-        <input {...register("Password1")} placeholder="Password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "only naver.com is allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message as string}</span>
+        <input
+          {...register("FirstName", { required: "write here", validate:{
+            noNico: (value) => value.includes("nico") ? "no nico allowed" : true
+          } })}
+          placeholder="FirstName"
+        />
+        <span>{errors?.FirstName?.message as string}</span>
+
+        <input
+          {...register("LastName", { required: "write here" })}
+          placeholder="LastName"
+        />
+        <span>{errors?.LastName?.message as string}</span>
+
+        <input
+          {...register("userName", { required: "write here", minLength: 10 })}
+          placeholder="userName"
+        />
+        <span>{errors?.userName?.message as string}</span>
+
+        <input
+          {...register("Password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.Password?.message as string}</span>
+
+        <input
+          {...register("Password1", {
+            required: "write here",
+            minLength: {
+              value: 5,
+              message: "Password is too shot",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.Password1?.message as string}</span>
+
         <button>Add</button>
+        <span>{errors.extraError?.message}</span>
       </form>
     </div>
   );
